47e1ad6b74ff17e6c4d366a9b391ff16480a3072
DeninaSiljegovic <dsiljegovi1@etf.unsa.ba>
2021-04-05 20:11:38 +0200

    item_kviz done izgled

--- app/src/main/java/ba/etf/rma21/projekat/data/models/Grupa.kt
+++ app/src/main/java/ba/etf/rma21/projekat/data/models/Grupa.kt
@@ -1,4 +1,7 @@
 package ba.etf.rma21.projekat.data.models
 
-data class Grupa(val naziv: String, val nazivPredmeta: String) {
+data class Grupa(
+    val naziv: String,
+    val nazivPredmeta: String
+) {
 }
--- app/src/main/java/ba/etf/rma21/projekat/data/models/Predmet.kt
+++ app/src/main/java/ba/etf/rma21/projekat/data/models/Predmet.kt
@@ -1,4 +1,7 @@
 package ba.etf.rma21.projekat.data.models
 
-data class Predmet(val naziv: String, val godina: Int) {
+data class Predmet(
+    val naziv: String,
+    val godina: Int
+) {
 }
