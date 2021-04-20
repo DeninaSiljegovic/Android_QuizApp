90b82057e4b68a805d432d1b277c5f1922dc0a9e
DeninaSiljegovic <dsiljegovi1@etf.unsa.ba>
2021-04-20 18:20:31 +0200

    fragment klase i layouts done

--- app/src/main/java/ba/etf/rma21/projekat/view/FragmentKvizovi.kt
+++ app/src/main/java/ba/etf/rma21/projekat/view/FragmentKvizovi.kt
@@ -1,4 +1,18 @@
 package ba.etf.rma21.projekat.view
 
-class FragmentKvizovi {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import ba.etf.rma21.projekat.R
+
+class FragmentKvizovi : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
+            inflater.inflate(R.layout.kvizovi_fragment, container, false)
+    companion object {
+        fun newInstance(): FragmentKvizovi = FragmentKvizovi()
+    }
+
 }
--- app/src/main/java/ba/etf/rma21/projekat/view/FragmentPredmeti.kt
+++ app/src/main/java/ba/etf/rma21/projekat/view/FragmentPredmeti.kt
@@ -1,4 +1,18 @@
 package ba.etf.rma21.projekat.view
 
-class FragmentPredmeti {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import ba.etf.rma21.projekat.R
+
+class FragmentPredmeti : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
+            inflater.inflate(R.layout.predmeti_fragment, container, false)
+    companion object {
+        fun newInstance(): FragmentPredmeti = FragmentPredmeti()
+    }
+
 }
