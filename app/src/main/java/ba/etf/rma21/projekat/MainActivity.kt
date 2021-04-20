3b0cb8dd742c39b7ee75f311da2bc2b100ba8fc7
DeninaSiljegovic <dsiljegovi1@etf.unsa.ba>
2021-04-20 21:32:51 +0200

    fragment kvizovi  - prebacen kod

--- app/src/main/java/ba/etf/rma21/projekat/MainActivity.kt
+++ app/src/main/java/ba/etf/rma21/projekat/MainActivity.kt
@@ -1,26 +1,13 @@
 package ba.etf.rma21.projekat
 
 
-import android.content.Intent
-import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
-import android.util.TypedValue
-import android.view.View
-import android.view.ViewGroup
-import android.widget.AdapterView
-import android.widget.AdapterView.OnItemSelectedListener
-import android.widget.ArrayAdapter
-import android.widget.Spinner
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import ba.etf.rma21.projekat.view.FragmentKvizovi
 import ba.etf.rma21.projekat.view.FragmentPredmeti
-import ba.etf.rma21.projekat.view.KvizListAdapter
-import ba.etf.rma21.projekat.viewmodel.KvizViewModel
 import ba.etf.rma21.projekat.viewmodel.PredmetViewModel
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
@@ -28,10 +15,10 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity(){
     private lateinit var bottomNavigation : BottomNavigationView
-    private lateinit var listaKvizova: RecyclerView
-    private lateinit var filterKvizova: Spinner
-    private lateinit var listaKvizovaAdapter: KvizListAdapter
-    private var kvizListViewModel = KvizViewModel()
+//    private lateinit var listaKvizova: RecyclerView
+//    private lateinit var filterKvizova: Spinner
+//    private lateinit var listaKvizovaAdapter: KvizListAdapter
+//    private var kvizListViewModel = KvizViewModel()
     private var predmetListViewModel = PredmetViewModel()
     private lateinit var upisDugme: FloatingActionButton
     private var odabranaGod : Int = 0
@@ -57,12 +44,7 @@ class MainActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        listaKvizova = findViewById(R.id.listaKvizova)
-        filterKvizova = findViewById(R.id.filterKvizova)
-        upisDugme = findViewById(R.id.upisDugme)
         bottomNavigation= findViewById(R.id.bottomNav)
-
 
         bottomNavigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
         //Defaultni fragment
@@ -70,100 +52,101 @@ class MainActivity : AppCompatActivity(){
         val kvizFragment = FragmentKvizovi.newInstance()
         openFragment(kvizFragment)
 
-        // Spinner Drop down elements
-        val categories: MutableList<String> = ArrayList()
-        categories.add("Svi moji kvizovi")
-        categories.add("Svi kvizovi")
-        categories.add("Urađeni kvizovi")
-        categories.add("Budući kvizovi")
-        categories.add("Prošli kvizovi")
+//        // Spinner Drop down elements
+//        val categories: MutableList<String> = ArrayList()
+//        categories.add("Svi moji kvizovi")
+//        categories.add("Svi kvizovi")
+//        categories.add("Urađeni kvizovi")
+//        categories.add("Budući kvizovi")
+//        categories.add("Prošli kvizovi")
+//
+//        // Creating adapter for spinner
+//        //val dataAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, categories)
+//        val dataAdapter:ArrayAdapter<String> = object: ArrayAdapter<String>(
+//            this,
+//            android.R.layout.simple_spinner_dropdown_item,
+//            categories
+//        ){
+//            override fun getDropDownView(
+//                position: Int,
+//                convertView: View?,
+//                parent: ViewGroup
+//            ): View {
+//                val view: TextView = super.getDropDownView(
+//                    position,
+//                    convertView,
+//                    parent
+//                ) as TextView
+//                // set item text size
+//                view.setTextSize(TypedValue.COMPLEX_UNIT_SP,15F)
+//
+//                // set selected item style
+//                if (position == filterKvizova.selectedItemPosition){
+//                    view.background = ColorDrawable(Color.parseColor("#E695B1"))
+//                    view.setTextColor(Color.parseColor("#2E2D88"))
+//                }
+//                return view
+//            }
+//        }
+//
+//
+//        // Drop down layout style - list view with radio button
+//        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+//
+//        // attaching data adapter to spinner
+//        filterKvizova.setAdapter(dataAdapter);
 
-        // Creating adapter for spinner
-        //val dataAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, categories)
-        val dataAdapter:ArrayAdapter<String> = object: ArrayAdapter<String>(
-            this,
-            android.R.layout.simple_spinner_dropdown_item,
-            categories
-        ){
-            override fun getDropDownView(
-                position: Int,
-                convertView: View?,
-                parent: ViewGroup
-            ): View {
-                val view: TextView = super.getDropDownView(
-                    position,
-                    convertView,
-                    parent
-                ) as TextView
-                // set item text size
-                view.setTextSize(TypedValue.COMPLEX_UNIT_SP,15F)
+//        //grid layout - da se prikazu fino kao mreza - linear je da idu ----
+//        listaKvizova.layoutManager = GridLayoutManager(
+//                this,
+//                2,
+//                GridLayoutManager.VERTICAL,
+//                false
+//        )
+//        listaKvizovaAdapter = KvizListAdapter(listOf())
+//        listaKvizova.adapter = listaKvizovaAdapter
+//
+//        filterKvizova.setOnItemSelectedListener(object : OnItemSelectedListener {
+//            override fun onItemSelected(parent: AdapterView<*>?, view: View, position: Int, id: Long) {
+//                //first,  we have to retrieve the item position as a string
+//                // then, we can change string value into integer
+//                val item_position = position.toString()
+//                val positonInt = Integer.valueOf(item_position)
+//                if(positonInt  == 0)  listaKvizovaAdapter.updateKvizove(kvizListViewModel.getMyKvizes())
+//                else if(positonInt == 1)  listaKvizovaAdapter.updateKvizove(kvizListViewModel.getAll())
+//                else if(positonInt == 2) listaKvizovaAdapter.updateKvizove(kvizListViewModel.getMyDone())
+//                else if(positonInt == 3) listaKvizovaAdapter.updateKvizove(kvizListViewModel.getMyFuture())
+//                else if(positonInt == 4) listaKvizovaAdapter.updateKvizove(kvizListViewModel.getMyNotTaken())
+//            }
+//
+//            override fun onNothingSelected(parent: AdapterView<*>?) { listaKvizovaAdapter.updateKvizove(kvizListViewModel.getAll()) }
+//        })
 
-                // set selected item style
-                if (position == filterKvizova.selectedItemPosition){
-                    view.background = ColorDrawable(Color.parseColor("#E695B1"))
-                    view.setTextColor(Color.parseColor("#2E2D88"))
-                }
-                return view
-            }
-        }
-
-
-        // Drop down layout style - list view with radio button
-        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-
-        // attaching data adapter to spinner
-        filterKvizova.setAdapter(dataAdapter);
-
-        //grid layout - da se prikazu fino kao mreza - linear je da idu ----
-        listaKvizova.layoutManager = GridLayoutManager(
-                this,
-                2,
-                GridLayoutManager.VERTICAL,
-                false
-        )
-        listaKvizovaAdapter = KvizListAdapter(listOf())
-        listaKvizova.adapter = listaKvizovaAdapter
-
-        filterKvizova.setOnItemSelectedListener(object : OnItemSelectedListener {
-            override fun onItemSelected(parent: AdapterView<*>?, view: View, position: Int, id: Long) {
-                //first,  we have to retrieve the item position as a string
-                // then, we can change string value into integer
-                val item_position = position.toString()
-                val positonInt = Integer.valueOf(item_position)
-                if(positonInt  == 0)  listaKvizovaAdapter.updateKvizove(kvizListViewModel.getMyKvizes())
-                else if(positonInt == 1)  listaKvizovaAdapter.updateKvizove(kvizListViewModel.getAll())
-                else if(positonInt == 2) listaKvizovaAdapter.updateKvizove(kvizListViewModel.getMyDone())
-                else if(positonInt == 3) listaKvizovaAdapter.updateKvizove(kvizListViewModel.getMyFuture())
-                else if(positonInt == 4) listaKvizovaAdapter.updateKvizove(kvizListViewModel.getMyNotTaken())
-            }
-
-            override fun onNothingSelected(parent: AdapterView<*>?) { listaKvizovaAdapter.updateKvizove(kvizListViewModel.getAll()) }
-        })
-
-        upisDugme.setOnClickListener{
-            upisDugmeClicked(it)
-        }
+//        upisDugme.setOnClickListener{
+//            upisDugmeClicked(it)
+//        }
     }//onCreate end
 
-     //"Go to Second Activity" button click
-    fun upisDugmeClicked(view: View?) {
-        val intent = Intent(this, UpisPredmetActivity::class.java)
-         intent.putExtra("selectedYear", odabranaGod.toString())
-        startActivityForResult(intent, 0)
-    }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
+//     //"Go to Second Activity" button click
+//    fun upisDugmeClicked(view: View?) {
+//        val intent = Intent(this, UpisPredmetActivity::class.java)
+//         intent.putExtra("selectedYear", odabranaGod.toString())
+//        startActivityForResult(intent, 0)
+//    }
 
-        // check that it is the SecondActivity with an OK result
-            if (resultCode == RESULT_OK) {
-                odabranaGod = data?.getStringExtra("selectedYear").toString().toInt()
-                kvizListViewModel.upisiKviz(data?.getStringExtra("grupa").toString()) //RADI OK
-                predmetListViewModel.upisi(data?.getStringExtra("predmet").toString(), data?.getStringExtra("godina").toString().toInt()+1)
-                listaKvizovaAdapter.updateKvizove(kvizListViewModel.getMyKvizes())
-                filterKvizova.setSelection(0)
-            }
-    }
+//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+//        super.onActivityResult(requestCode, resultCode, data)
+//
+//        // check that it is the SecondActivity with an OK result
+//            if (resultCode == RESULT_OK) {
+//                odabranaGod = data?.getStringExtra("selectedYear").toString().toInt()
+//                kvizListViewModel.upisiKviz(data?.getStringExtra("grupa").toString()) //RADI OK
+//                predmetListViewModel.upisi(data?.getStringExtra("predmet").toString(), data?.getStringExtra("godina").toString().toInt()+1)
+//                listaKvizovaAdapter.updateKvizove(kvizListViewModel.getMyKvizes())
+//                filterKvizova.setSelection(0)
+//            }
+//    }
 
     private fun openFragment(fragment: Fragment) {
         val transaction = supportFragmentManager.beginTransaction()
--- app/src/main/java/ba/etf/rma21/projekat/view/FragmentKvizovi.kt
+++ app/src/main/java/ba/etf/rma21/projekat/view/FragmentKvizovi.kt
@@ -1,18 +1,112 @@
 package ba.etf.rma21.projekat.view
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import ba.etf.rma21.projekat.R
+import ba.etf.rma21.projekat.viewmodel.KvizViewModel
+
 
 class FragmentKvizovi : Fragment() {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
-            inflater.inflate(R.layout.kvizovi_fragment, container, false)
-    companion object {
-        fun newInstance(): FragmentKvizovi = FragmentKvizovi()
+    private lateinit var listaKvizova: RecyclerView
+    private lateinit var filterKvizova: Spinner
+    private lateinit var listaKvizovaAdapter: KvizListAdapter
+    private var kvizListViewModel = KvizViewModel()
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        var view = inflater.inflate(R.layout.kvizovi_fragment, container, false)
+        listaKvizova = view.findViewById(R.id.listaKvizova)
+        filterKvizova = view.findViewById(R.id.filterKvizova)
+
+        // Spinner Drop down elements
+        val categories: MutableList<String> = ArrayList()
+        categories.add("Svi moji kvizovi")
+        categories.add("Svi kvizovi")
+        categories.add("Urađeni kvizovi")
+        categories.add("Budući kvizovi")
+        categories.add("Prošli kvizovi")
+
+        // Creating adapter for spinner
+        //val dataAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, categories)
+        val dataAdapter:ArrayAdapter<String> = object: ArrayAdapter<String>(
+            view.context,
+            android.R.layout.simple_spinner_dropdown_item,
+            categories
+        ){
+            override fun getDropDownView(
+                position: Int,
+                convertView: View?,
+                parent: ViewGroup
+            ): View {
+                val view: TextView = super.getDropDownView(
+                    position,
+                    convertView,
+                    parent
+                ) as TextView
+                // set item text size
+                view.setTextSize(TypedValue.COMPLEX_UNIT_SP,15F)
+
+                // set selected item style
+                if (position == filterKvizova.selectedItemPosition){
+                    view.background = ColorDrawable(Color.parseColor("#E695B1"))
+                    view.setTextColor(Color.parseColor("#2E2D88"))
+                }
+                return view
+            }
+        }
+
+        // Drop down layout style - list view with radio button
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        // attaching data adapter to spinner
+        filterKvizova.setAdapter(dataAdapter);
+
+        //grid layout - da se prikazu fino kao mreza - linear je da idu ----
+        listaKvizova.layoutManager = GridLayoutManager(
+                activity,
+                2,
+                GridLayoutManager.VERTICAL,
+                false
+        )
+        listaKvizovaAdapter = KvizListAdapter(listOf())
+        listaKvizova.adapter = listaKvizovaAdapter
+
+        filterKvizova.setOnItemSelectedListener(object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View, position: Int, id: Long) {
+                //first,  we have to retrieve the item position as a string
+                // then, we can change string value into integer
+                val item_position = position.toString()
+                val positonInt = Integer.valueOf(item_position)
+                if(positonInt  == 0)  listaKvizovaAdapter.updateKvizove(kvizListViewModel.getMyKvizes())
+                else if(positonInt == 1)  listaKvizovaAdapter.updateKvizove(kvizListViewModel.getAll())
+                else if(positonInt == 2) listaKvizovaAdapter.updateKvizove(kvizListViewModel.getMyDone())
+                else if(positonInt == 3) listaKvizovaAdapter.updateKvizove(kvizListViewModel.getMyFuture())
+                else if(positonInt == 4) listaKvizovaAdapter.updateKvizove(kvizListViewModel.getMyNotTaken())
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) { listaKvizovaAdapter.updateKvizove(kvizListViewModel.getAll()) }
+        })
+
+
+        return view
     }
+
+        companion object {
+            fun newInstance(): FragmentKvizovi = FragmentKvizovi()
+        }
+
 
 }
